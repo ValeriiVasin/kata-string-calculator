da60613fd1aa5fd8b439c08c3b39a96c5b3698cb
Valerii Vasin <valerii.vasin@gmail.com>
2016-05-30 19:52:15 +0200

    Solve simple test cases

--- kata.js
+++ kata.js
@@ -1,3 +1,11 @@
-export function add() {
-  return true;
+export function add(...numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  return numbers
+    .map(n => n.trim())
+    .filter(n => n !== '')
+    .map(Number)
+    .reduce((acc, n) => acc + n, 0);
 }
--- spec/kata.spec.js
+++ spec/kata.spec.js
@@ -1,7 +1,23 @@
 import { add } from '../kata';
 
 describe('kata', () => {
-  it('works', () => {
-    expect(add()).toBe(true);
+  it('returns zero if none arguments provided', () => {
+    expect(add()).toBe(0);
+  });
+
+  it('return zero if empty string provided', () => {
+    expect(add('')).toBe(0);
+  });
+
+  it('works for one number', () => {
+    expect(add('123')).toBe(123);
+  });
+
+  it('works for empty strings in input', () => {
+    expect(add('', '1', '123')).toBe(124);
+  });
+
+  it('works for untrimmed input', () => {
+    expect(add('       ', '1      ', '  123   ')).toBe(124);
   });
 });
